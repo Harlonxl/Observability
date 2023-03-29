fbbf2fcb5080f5ddb58adfcfe8eda262df591f61
Harlon <harlonxl@gmail.com>
2023-03-30 01:12:39 +0800

    feat: 若依增加nginx链路

--- ruoyi-terraform-image/dist/start.sh
+++ ruoyi-terraform-image/dist/start.sh
@@ -5,4 +5,9 @@ $(</data/nginx/web/dist/index.html)
 EOF
 " 2> /dev/null > /data/nginx/web/dist/index.html
 
+eval "cat <<EOF
+$(</etc/nginx/dd-config.json)
+EOF
+" 2> /dev/null > /etc/nginx/dd-config.json
+
 nginx -g "daemon off;"
